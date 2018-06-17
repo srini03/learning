bbd3ddff806d3249f2d9b19bcd524984668b5684
srini03 <srini03@users.noreply.github.com>
2018-06-17 00:24:59 -0400

    Add files via upload
    oracle truncate example

---
+++ python_truncate.py
@@ -0,0 +1,8 @@
+from __future__ import print_function
+
+import cx_Oracle
+import db_config
+
+con = cx_Oracle.connect(db_config.user, db_config.pw, db_config.dsn)
+cur = con.cursor()
+cur.execute("truncate table test.emp_test")
